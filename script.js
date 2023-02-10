123bc65dc928d90469d70a183b55f16c5fab065f
ArdFlamingo <110302468+ArdFlamingo@users.noreply.github.com>
2023-02-09 19:51:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-document.querySelector("h1").onmouseover = event => {
-  let interations = 0;
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
   
-  const interval = setInterval(() => {
-    event.target.innterText = event.target.innerText.split("")
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
       .map((letter, index) => {
-      if (index < iterations) {
-        return event.target.dataset.value[index];
-      }
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
       
-      return letters[Math.floor(Math.random() * 26)];
-    })
+        return letters[Math.floor(Math.random() * 26)]
+      })
       .join("");
     
-    if (iterations >= event.target.dataset.value.length) {
-      clearInterval(iterval);
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
     }
     
-    iterations += 1 / 3;
-  }, 1000);
+    iteration += 1 / 3;
+  }, 30);
 }
