b476a2b4617256125c70289fdc8e176f429f5cfb
ArdFlamingo <110302468+ArdFlamingo@users.noreply.github.com>
2023-02-13 18:15:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,35 +2,41 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let interval = null;
 
-window.onload = function ()
+window.onload = () =>
 {
-	document.querySelector("h1").onmouseover = event =>
-	{  
-	  let iteration = 0;
-	  
-	  clearInterval(interval);
-	  
-	  interval = setInterval(() =>
-	  {
-		event.target.innerText = event.target.innerText
-		  .split("")
-		  .map((letter, index) =>
-		  {
-			if(index < iteration)
-			{
-			  return event.target.dataset.value[index];
+	let element = document.querySelector("h1");
+	let innerHTML = element.innerHTML;
+	
+	action = () =>
+	{	
+		let iteration = 0;
+		
+		clearInterval(interval);
+		
+		interval = setInterval(() =>
+		{
+			element.innerText = element.innerText
+				.split("")
+				.map((letter, index) =>
+				{
+					if(index < iteration)
+					{
+						return element.dataset.value[index];
+					}
+				
+					return letters[Math.floor(Math.random() * 26)];
+				})
+				.join("");
+			
+			if(iteration >= element.dataset.value.length)
+			{ 
+				clearInterval(interval);
+				element.innerHTML = innerHTML;
 			}
-		  
-			return letters[Math.floor(Math.random() * 26)];
-		  })
-		  .join("");
-		
-		if(iteration >= event.target.dataset.value.length)
-		{ 
-		  clearInterval(interval);
-		}
-		
-		iteration += 1 / 2;
-	  }, 4);
-	}
+			
+			iteration += 1 / 2;
+		}, 1);
+	};
+	
+	action();
 }
